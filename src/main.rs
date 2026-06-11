e0c562ffbc2320cb09c16272ff234d927b47e1eb
Spheniscine <spheniscine@gmail.com>
2026-06-11 21:39:59 +0800

    testing out "mystical" suit set exclusive for this variant

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use dioxus::prelude::*;
+use glam::Vec2;
+use strum::IntoEnumIterator;
+
+use crate::{components::CardComponent, game::{Card, Skin, Suit}};
+
+mod game;
+mod components;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+
+// altered version of KaTeX_Main to include filled "red" suits
+const KATEX_SUITS: Asset = asset!("/assets/KaTeX_Suits.woff2");
+
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
@@ -11,8 +22,32 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+        }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "anonymous",
+        }
+        document::Link {
+            href: "https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@300..700&family=Noto+Sans+Symbols+2&family=Noto+Sans+Symbols:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+            rel: "stylesheet",
+        }
+
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Style {
+            r#"
+            @font-face {{
+                font-family: KaTeX_Main;
+                font-style: normal;
+                font-weight: 700;
+                src: url({KATEX_SUITS}) format("woff2");
+            }}
+            "#,
+        }
         Hero {}
 
     }
@@ -20,17 +55,18 @@ fn App() -> Element {
 
 #[component]
 pub fn Hero() -> Element {
+    let skin = Skin::default();
+    let iter = Suit::iter().enumerate();
     rsx! {
         div {
             id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            for (i, suit) in iter {
+                CardComponent { 
+                    position: Vec2::new(10., 10. + 15. * i as f32),
+                    width: 11.,
+                    card: Card { rank: 9, suit, },
+                    skin,
+                }
             }
         }
     }
